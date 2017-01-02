d10a220f95d60053f6883f2caff77561e12a097e
Mukesh Kumar <singh88rajat@gmail.com>
2017-01-02 23:56:09 +0530

    Emptly Variable value

---
+++ src/example.java
@@ -0,0 +1,15 @@
+
+public class example {
+	int x;         
+	int y;
+	String name;
+	public static void main (String args    [])
+	  {
+		
+		example pnt = new example();
+		System.out.println("pnt is "   + pnt.name + " " + pnt.x+" " + pnt.y);
+	  }
+}
+/*
+d. The  program prints  pnt is null 0 0
+*/
